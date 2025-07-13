e97e4f16e1e25ee1a30d99c0e09eebcae9d0444a
lorenzopaoria <pralnz01s24h163s@studium.unict.it>
2025-07-13 11:48:35 +0200

    update_java_cofig_on_ec2 commit, action in corso...

--- mvnProject-Client/src/main/java/com/example/DatabaseClient.java
+++ mvnProject-Client/src/main/java/com/example/DatabaseClient.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Scanner; // Import for reading user input
 
 public class DatabaseClient {
-    private static final String SERVER_HOST = "musicapp-nlb-ef8f598cab406587.elb.us-east-1.amazonaws.com";
+    private static final String SERVER_HOST = "3.83.204.53";
     private static final int SERVER_PORT = 8080;
 
     private static final List<UserTest> TEST_USERS = new ArrayList<>();
