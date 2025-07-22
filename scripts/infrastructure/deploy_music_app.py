2eeed867cf632d8f0f3aa9ccf4d64bacef8628e1
lorenzopaoria <pralnz01s24h163s@studium.unict.it>
2025-07-22 09:41:55 +0200

    update_java_cofig_on_ec2 commit, action in corso...

--- scripts/infrastructure/deploy_music_app.py
+++ scripts/infrastructure/deploy_music_app.py
@@ -733,6 +733,22 @@ def main():
             data_sql=dati_sql_content
         )
 
+        # rimuovo la regola di ingresso da 0.0.0.0/0 per la porta 5432 su RDS
+        # try:
+        #     ec2_client.revoke_security_group_ingress(
+        #         GroupId=rds_security_group_id,
+        #         IpProtocol='tcp',
+        #         FromPort=5432,
+        #         ToPort=5432,
+        #         CidrIp='0.0.0.0/0'
+        #     )
+        #     print("[SUCCESS] Regola di ingresso 0.0.0.0/0 rimossa dal Security Group RDS.")
+        # except ClientError as e:
+        #     if 'InvalidPermission.NotFound' in str(e):
+        #         print("[INFO] Regola di ingresso 0.0.0.0/0 già rimossa dal Security Group RDS.")
+        #     else:
+        #         print(f"[ERROR] Errore nella rimozione della regola di ingresso: {e}")
+
         # STEP 6: configurazione delle notifiche SNS
         topic_name = 'musicapp-server-setup-complete'
         email_address = 'lorenzopaoria@icloud.com'
